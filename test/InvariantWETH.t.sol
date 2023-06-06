b2cdf7b9fcd89683c27c830e0edd52513eb61933
Vladyslav Kotsiuba <vladyslav.kotsiuba@gmail.com>
2023-06-06 16:00:35 +0300

    Add invariant weth test

--- test/InvariantWETH.t.sol
+++ test/InvariantWETH.t.sol
@@ -3,4 +3,16 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
-import "@openzeppelin/contracts/token/ERC20/";
+import "solmate/tokens/WETH.sol";
+
+contract InvariantWETHTest is Test {
+    WETH public weth;
+
+    function setUp() public {
+        weth = new WETH();
+    }
+
+    function invariant_BalanceIsAlwaysZero() public {
+        assertEq(weth.balanceOf(address(this)), 0);
+    }
+}
